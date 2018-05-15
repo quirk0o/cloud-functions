deaafd17a3ef762f2ed7b7a743044a19a03b6cba
Beata Obrok <beata.obrok@u2i.com>
2018-05-15 12:23:24 +0200

    [aws] feat: download and upload with streaming (#19)
    * feat: extract async package
    * feat: extract benchmark package
    
    * Publish
    
     - @quirk0.o/async@1.0.1
     - @quirk0.o/benchmark@1.0.1
     - @quirk0.o/serverless-research-gcf@2.0.4
    
    * [google] fix: internal package imports
    * [aws] refactor: remove bin and build_native.sh
    * [aws] feat: transfer with streaming

--- packages/google/index.js
+++ packages/google/index.js
@@ -2,7 +2,9 @@ const runtimeConfig = require('cloud-functions-runtime-config')
 const storage = require('@google-cloud/storage')()
 const fs = require('fs')
 const stream = require('stream')
-const streamToPromise = require('cloud-functions-common').streamToPromise
+const streamToPromise = require('@quirk0.o/cloud-functions-common').streamToPromise
+const Benchmark = require('@quirk0.o/benchmark')
+const {logP} = require('@quirk0.o/async')
 const env = require('./environment')
 
 const CONFIG_KEY = `serverless-research-config`
@@ -11,18 +13,7 @@ const OUTPUT_BUCKET_CONFIG_KEY = 'buckets/output'
 
 const config = (key) => () => runtimeConfig.getVariable(CONFIG_KEY, key)
 
-const tapP = (fn) => (value) => {
-  fn(value)
-  return value
-}
-const tapError = (fn) => (value) => {
-  fn(value)
-  return Promise.reject(value)
-}
-const logP = (fn) => tapP((value) => console.log(fn(value)))
-const pipeP = (...fns) => fns.reduce((prev, curr) => prev.then(curr), Promise.resolve())
-
-const timestampedFileName = () => `random_${(new Date()).toISOString()}`
+const timestampedFileName = () => `transfer_${(new Date()).toISOString()}`
 const file = (bucket, fileName) => storage.bucket(bucket).file(fileName)
 const readFile = (bucket, fileName) => {
   const fileStream = file(bucket, fileName).createReadStream()
@@ -49,47 +40,6 @@ const writeFile = (bucket, fileName) => {
   generator.pipe(writeStream)
 }
 
-class Benchmark {
-  constructor() {
-    this._keys = []
-    this._queue = []
-    this._response = {}
-    this._time = {}
-  }
-
-  do(key) {
-    this._keys.push(key)
-    return (...fns) => {
-      this._queue.push(this._task(key, fns))
-      return this
-    }
-  }
-
-  json() {
-    return pipeP(...this._queue)
-      .then(() => Object.assign(
-        {
-          ts: (new Date()).toString(),
-          time: this._time
-        },
-        this._keys
-          .map(key => ({[key]: this._response[key]}))
-          .reduce((acc, response) => Object.assign(acc, response), {})
-      ))
-  }
-
-  _task(key, fns) {
-    return () => {
-      let hrtime = process.hrtime()
-      return pipeP(...fns)
-        .then(() => this._time[key] = process.hrtime(hrtime))
-        .then(logP(() => `${key} finished`))
-        .catch(tapError(logP(err => `${key} error: ${err}`)))
-        .catch(error => this._response[key] = {error})
-    }
-  }
-}
-
 exports.transfer = (request, response) => {
   const size = request.body.size
   const inputFileName = request.body.fileName
@@ -97,11 +47,16 @@ exports.transfer = (request, response) => {
   new Benchmark()
     .do('download')(
       config(INPUT_BUCKET_CONFIG_KEY),
-      (bucket) => readFile(bucket, inputFileName)
+      logP(bucket => `Downloading gs://${bucket}/${inputFileName}`),
+      (bucket) => readFile(bucket, inputFileName),
+      logP(() => `Finished downloading`)
     )
     .do('upload')(
       config(OUTPUT_BUCKET_CONFIG_KEY),
-      (bucket) => writeFile(bucket, timestampedFileName())
+      (bucket) => [bucket, timestampedFileName()],
+      logP(([bucket, fileName]) => `Uploading to gs://${bucket}/${fileName}`),
+      ([bucket, fileName]) => writeFile(bucket, fileName),
+      logP(() => `Finished uploading`)
     )
     .json()
     .then(logP(json => `Finished: ${JSON.stringify(json)}`))
