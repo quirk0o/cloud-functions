7230d2abedf4b2e3a76516881ef34545294b8efc
Beata Obrok <beata@obrok.eu>
2018-05-15 12:09:01 +0200

    [azure] refactor: remove unused files

--- packages/azure/build_native.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker run --rm dockcross/windows-x64 > ./dockcross
-chmod +x ./dockcross
-mkdir -p functions/hello
-./dockcross -- sh -c '$CC -std=c99 src/random.c src/miniz.c src/hello.c  -o '$PROVIDER'/functions/hello/hello.exe'
-
