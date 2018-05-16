3da4c0a7919eed6cf8c33fba1317c3745b639b4b
Beata Obrok <beata@obrok.eu>
2018-05-16 10:07:03 +0200

    [azure] feat: download and upload with streaming

--- packages/azure/handler.js
+++ packages/azure/handler.js
@@ -1,49 +1,69 @@
 const Promise = require('bluebird')
 const azure = Promise.promisifyAll(require('azure-storage'))
-const ResponseBuilder = require('@quirk0.o/cloud-functions-common').ResponseBuilder
+const path = require('path')
+const fs = require('fs')
+const stream = require('stream')
+const Benchmark = require('@quirk0.o/benchmark')
+const {logP} = require('@quirk0.o/async')
 
 const STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING
 const OUTPUT_CONTAINER = process.env.AZURE_STORAGE_CONTAINER
-const INPUT_CONTAINER = process.env.AZURE_STORAGE_FILE_CONTAINER
+const INPUT_CONTAINER = process.env.AZURE_STORAGE_INPUT_CONTAINER
 
 const blobService = azure.createBlobService(STORAGE_CONNECTION_STRING)
 
-function downloadRequest(context, fileName) {
-  context.log(`Downloading from azs://${INPUT_CONTAINER}/${fileName}`)
-
-  if (!fileName) return Promise.reject('Missing fileName in event data!')
-  return blobService.getBlobToTextAsync(INPUT_CONTAINER, fileName)
+const timestampedFileName = () => `transfer_${(new Date()).toISOString()}`
+const readFile = (functionDirectory, container, fileName) => {
+  const writer = fs.createWriteStream(path.resolve(functionDirectory, 'input.dat'))
+  return blobService.getBlobToStreamAsync(container, fileName, writer)
 }
 
-function uploadRequest(context, data) {
-  const fileName = `/random_${(new Date()).toISOString()}`
+const writeFile = (container, fileName) => {
+  const generator = new stream.Readable()
+  const size = 64 * 1024
+  const chunkSize = 16384
+  let i = 1
 
-  context.log(`Uploading to azs://${OUTPUT_CONTAINER}/${fileName}`)
+  generator._read = () => {
+    if (i > size) {
+      return generator.push(null)
+    }
+    generator.push('\0'.repeat(chunkSize))
+    i += chunkSize
+  }
 
-  if (!data) return Promise.reject('No data to upload!')
-  return blobService.createContainerIfNotExistsAsync(OUTPUT_CONTAINER, {
-    publicAccessLevel: 'blob'
-  })
-    .then(() =>
-      blobService.createBlockBlobFromTextAsync(OUTPUT_CONTAINER, fileName, data)
+  return blobService.createBlockBlobFromStreamAsync(
+    container,
+    fileName,
+    generator,
+    size,
+    {blockIdPrefix: 'block'}
+  )
+}
+
+module.exports.transfer = (context, request) => {
+  context.log('hello')
+  const size = request.body.size
+  const inputFileName = request.body.fileName
+  const functionDirectory = context.executionContext.functionDirectory
+
+  new Benchmark()
+    .do('download')(
+      logP(() => `Downloading azs://${INPUT_CONTAINER}/${inputFileName}`, context),
+      () => readFile(functionDirectory, INPUT_CONTAINER, inputFileName),
+      logP(() => `Finished downloading`, context)
     )
-}
-
-module.exports.hello = function (context, req) {
-  const responseBuilder = new ResponseBuilder()
-
-  responseBuilder.download(downloadRequest(context, req.body.fileName))
-    .then((data) => {
-      return responseBuilder.upload(uploadRequest(context, data))
-    })
-    .then(() => {
-      context.res = responseBuilder.toJSON()
-      context.done()
-    })
-    .catch(() => {
-      context.res = responseBuilder.toJSON()
+    .do('upload')(
+      timestampedFileName,
+      logP((fileName) => `Uploading to azs://${OUTPUT_CONTAINER}/${fileName}`, context),
+      (fileName) => writeFile(OUTPUT_CONTAINER, fileName),
+      logP(() => `Finished uploading`, context)
+    )
+    .json()
+    .then(logP(json => `Finished: ${JSON.stringify(json)}`, context))
+    .then(json => {
+      context.log(json)
+      context.res = json
       context.done()
     })
 }
-
-
