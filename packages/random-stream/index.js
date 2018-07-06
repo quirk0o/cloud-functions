c833edc8e731ae065803e216ab897fdc4b874f14
Beata Obrok <beata.obrok@u2i.com>
2018-07-06 21:00:20 +0200

    fix: random stream (#47)
    * fix: randomString returning array instead of strings
    * fix: randomStream generating odd-sized chunks

--- packages/random-stream/index.js
+++ packages/random-stream/index.js
@@ -3,15 +3,17 @@ const stream = require('stream')
 const randomString = require('@quirk0.o/random-string')
 
 module.exports = (size, maxChunkSize) => {
-  let i = 1
+  let i = 0
 
   const generator = new stream.Readable({
     read() {
-      if (i > size) {
+      if (i >= size) {
         return generator.push(null)
       }
 
-      const chunkSize = i + maxChunkSize <= size ? maxChunkSize : size - i
+      const chunkSize = i + maxChunkSize <= size
+        ? maxChunkSize
+        : size - i
       generator.push(randomString(chunkSize))
       i += chunkSize
     }
