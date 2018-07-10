e012933dbca900cb08d88195e9b15c29a18335e8
Beata Obrok <beata.obrok@u2i.com>
2018-07-10 17:37:21 +0200

    google: fix: high bucket usage (#55)

--- packages/google/index.js
+++ packages/google/index.js
@@ -1,10 +1,12 @@
+const fs = require('fs')
+const bytes = require('bytes')
 const runtimeConfig = require('cloud-functions-runtime-config')
 const storage = require('@google-cloud/storage')()
-const fs = require('fs')
-const stream = require('stream')
-const streamToPromise = require('@quirk0.o/cloud-functions-common').streamToPromise
+
+const {streamToPromise} = require('@quirk0.o/cloud-functions-common')
 const Benchmark = require('@quirk0.o/benchmark')
 const {logP} = require('@quirk0.o/async')
+const randomStream = require('@quirk0.o/random-stream')
 const env = require('./environment')
 
 const CONFIG_KEY = `serverless-research-config`
@@ -13,68 +15,64 @@ const OUTPUT_BUCKET_CONFIG_KEY = 'buckets/output'
 
 const config = (key) => () => runtimeConfig.getVariable(CONFIG_KEY, key)
 
-const timestampedFileName = () => `transfer_${(new Date()).toISOString()}`
+const outputFileName = (memory, sizeStr) => `transfer_${memory}_${sizeStr}`
+const inputFileName = (sizeStr) => `${sizeStr}.dat`
+const outputFileSize = (sizeStr) => bytes.parse(sizeStr)
+
 const file = (bucket, fileName) => storage.bucket(bucket).file(fileName)
 const readFile = (bucket, fileName) => {
   const fileStream = file(bucket, fileName).createReadStream()
   const writer = fs.createWriteStream('/tmp/input.dat')
-
   return streamToPromise(fileStream.pipe(writer))
 }
 
-const writeFile = (bucket, fileName) => {
+const writeFile = (bucket, fileName, size) => {
   const writeStream = file(bucket, fileName).createWriteStream()
-  const size = 64 * 1024 * 1024
   const chunkSize = size / 8
-  let i = 1
-
-  const generator = new stream.Readable({
-    read(reqSize) {
-      console.log(`${process.memoryUsage().rss / 1024 / 1024} MB`)
-      if (i > size) {
-        return generator.push(null)
-      }
-      generator.push('\0'.repeat(reqSize))
-      i += chunkSize
-    }
-  })
-  generator.pause()
-
+  const generator = randomStream(size, chunkSize)
   return streamToPromise(generator.pipe(writeStream))
 }
 
-exports.transfer = (request, response) => {
-  const size = request.body.size
-  const inputFileName = request.body.fileName
-
-  new Benchmark()
-    .do('download')(
-      logP(() => `${process.memoryUsage().rss / 1024 / 1024} MB`),
-      config(INPUT_BUCKET_CONFIG_KEY),
-      logP(bucket => `Downloading gs://${bucket}/${inputFileName}`),
-      (bucket) => readFile(bucket, inputFileName),
-      logP(() => `Finished downloading`),
-      logP(() => `${process.memoryUsage().rss / 1024 / 1024} MB`)
-    )
-    .do('upload')(
-      config(OUTPUT_BUCKET_CONFIG_KEY),
-      (bucket) => [bucket, timestampedFileName()],
-      logP(([bucket, fileName]) => `Uploading to gs://${bucket}/${fileName}`),
-      ([bucket, fileName]) => writeFile(bucket, fileName),
-      logP(() => `Finished uploading`)
-    )
-    .json()
-    .then(logP(json => `Finished: ${JSON.stringify(json)}`))
-    .then(json => response.status(200).json(json))
+const validateReq = (body) => {
+  if (!body.size) throw new Error('Missing file size in event data.')
+  return body
 }
 
-exports['transfer-128'] = exports.transfer
-exports['transfer-256'] = exports.transfer
-exports['transfer-512'] = exports.transfer
-exports['transfer-1024'] = exports.transfer
-exports['transfer-2048'] = exports.transfer
-exports['transfer-dev-128'] = exports.transfer
-exports['transfer-dev-256'] = exports.transfer
-exports['transfer-dev-512'] = exports.transfer
-exports['transfer-dev-1024'] = exports.transfer
-exports['transfer-dev-2048'] = exports.transfer
+exports.transfer = (memory) => (request, response) => {
+  try {
+    validateReq(request.body)
+    const {size} = request.body
+
+    new Benchmark()
+      .do('download')(
+        config(INPUT_BUCKET_CONFIG_KEY),
+        (bucket) => [bucket, inputFileName(size)],
+        logP(([bucket, fileName]) => `Downloading gs://${bucket}/${fileName}`),
+        ([bucket, fileName]) => readFile(bucket, fileName),
+        logP(() => `Finished downloading`)
+      )
+      .do('upload')(
+        config(OUTPUT_BUCKET_CONFIG_KEY),
+        (bucket) => [bucket, outputFileName(memory, size), outputFileSize(size)],
+        logP(([bucket, fileName]) => `Uploading to gs://${bucket}/${fileName}`),
+        ([bucket, fileName, fileSize]) => writeFile(bucket, fileName, fileSize),
+        logP(() => `Finished uploading`)
+      )
+      .json()
+      .then(logP(json => `Finished: ${JSON.stringify(json)}`))
+      .then(json => response.status(200).json(json))
+  } catch (e) {
+    response.status(200).json({error: e.message})
+  }
+}
+
+exports['transfer-128'] = exports.transfer('128')
+exports['transfer-256'] = exports.transfer('256')
+exports['transfer-512'] = exports.transfer('512')
+exports['transfer-1024'] = exports.transfer('1024')
+exports['transfer-2048'] = exports.transfer('2048')
+exports['transfer-dev-128'] = exports.transfer('128')
+exports['transfer-dev-256'] = exports.transfer('256')
+exports['transfer-dev-512'] = exports.transfer('512')
+exports['transfer-dev-1024'] = exports.transfer('1024')
+exports['transfer-dev-2048'] = exports.transfer('2048')
