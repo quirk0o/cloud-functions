85d619b2a69dbc2fd00e6a3b2a97cb55100837d9
Beata Obrok <beata@obrok.eu>
2018-10-28 12:44:27 +0100

    [ibm] feat: download and upload

---
+++ packages/ibm/services/download/handler.js
@@ -0,0 +1,73 @@
+require('dotenv').config()
+
+const fs = require('fs')
+const IBM = require('ibm-cos-sdk')
+
+const {streamToPromise, MissingParamError} = require('@quirk0.o/cloud-functions-common')
+const Benchmark = require('@quirk0.o/benchmark')
+const {logP} = require('@quirk0.o/async')
+
+const cosConfig = {
+  endpoint: process.env.storageEndpoint,
+  apiKeyId: process.env.apiKeyId,
+  ibmAuthEndpoint: process.env.authEndpoint,
+  serviceInstanceId: process.env.serviceInstanceId
+}
+const cos = new IBM.S3(cosConfig)
+
+const INPUT_BUCKET = process.env.inputBucket
+const TEMPORARY_FILE = '/tmp/input.dat'
+
+const inputFileName = (sizeStr) => `${sizeStr}.dat`
+
+const file = (bucket, fileName, options) => Object.assign({Bucket: bucket, Key: fileName}, options)
+const readFile = (bucket, fileName) => {
+  const params = file(bucket, fileName)
+  return cos.headObject(params)
+    .promise()
+    .then(() => {
+      const fileStream = cos
+        .getObject(params)
+        .on('error', Promise.reject)
+        .createReadStream()
+      const writer = fs.createWriteStream(TEMPORARY_FILE)
+
+      return streamToPromise(fileStream.pipe(writer))
+    })
+    .catch((error) => {
+      if (error.statusCode === 404) {
+        return Promise.reject(`No such file: cos://${bucket}/${fileName}.`)
+      }
+      return Promise.reject(error)
+    })
+}
+
+const requireSize = (params) => {
+  if (!params.size) {
+    throw new MissingParamError('size')
+  }
+  return params.size
+}
+
+exports.download = (params) => {
+  try {
+    const size = requireSize(params)
+
+    return new Benchmark()
+      .do('download')(
+        () => inputFileName(size),
+        logP((fileName) => `Downloading cos://${INPUT_BUCKET}/${fileName}`),
+        (fileName) => readFile(INPUT_BUCKET, fileName),
+        logP(() => `Finished downloading`)
+      )
+      .json()
+      .then(logP((json) => `Finished: ${JSON.stringify(json)}`))
+  }
+  catch (e) {
+    if (e instanceof MissingParamError) {
+      return {status: 400, error: e.message}
+    }
+    return {status: 500, error: e.message}
+  }
+}
+
---
+++ packages/ibm/test/download.spec.js
@@ -0,0 +1,35 @@
+const request = require('axios')
+
+const baseUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api'
+const apiId = '79fcf6ebf02e88f3cfac59ae37ec551b193961b2b82d1e0bbce94f6c69323150'
+const service = `serverless-research`
+const fn = `download-512`
+const url = `${baseUrl}/${apiId}/${service}/${fn}`
+const size = '1MB'
+
+jest.setTimeout(20000)
+
+describe('download', () => {
+  it('tests download speed', async () => {
+    try {
+      const response = await request.post(url, {size})
+      expect(response.status).toEqual(200)
+
+      const data = response.data
+      console.log(data)
+      expect(data.ts).toBeDefined()
+      expect(data.download && data.download.error).not.toBeDefined()
+      expect(data.time).toBeDefined()
+      expect(data.time.download).toBeInstanceOf(Array)
+      expect(data.time.download).toHaveLength(2)
+    } catch (error) {
+      if (error.response) {
+        return Promise.reject(`Request failed with status code: ${error.response.status} ${error.response.statusText}.\nThe response was: \n${JSON.stringify(error.response.data)}`)
+      } else if (error.request) {
+        return Promise.reject(`Request failed: ${error.message}`)
+      } else {
+        return Promise.reject(`Unexpected error: ${error.message}`)
+      }
+    }
+  })
+})
